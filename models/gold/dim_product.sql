8eeb1d40a92abe12a3edf16a183d34cf71bff1f3
Nick Hudson <nhudson.c@revolutiondance.com>
2026-08-14 22:56:08 +0000

    Added row source table and

--- models/gold/dim_product.sql
+++ models/gold/dim_product.sql
@@ -26,10 +26,16 @@ SELECT
         ,CPSCStyleCompliant
         ,CPSCStyleCompliantDate
         ,CPSCStyleExpiration
+        ,is_current
+        , version_start_date
+        , version_end_date
+        , dbt_updated_at
+        , etl_insert_datetime
     )
     , row_number() over (
         partition by product_key
         order by effective_start_datetime desc
     ) as version_number
+    , case when version_number = 1 then 1 else 0 end as is_current_row
 FROM
     {{ref("silver_snapshot_dim_product")}} snap_p
