aacebbc8ddde00baf1259a42b5528dfdb3a8882c
Nick Hudson <nhudson.c@revolutiondance.com>
2026-09-02 19:21:38 +0000

    looked at target schema rather than target.name

--- macros/generate_schema_name.sql
+++ macros/generate_schema_name.sql
@@ -1,16 +1,9 @@
 {# macros/generate_schema_name.sql #}
 
 {% macro generate_schema_name(custom_schema_name, node) -%}
-    {# If we are in production, output exactly the custom schema name (e.g., 'silver') #}
-    {%- if target.name == 'prod' or target.name == 'production' -%}
-        {{ custom_schema_name | trim }}
-    
-    {# If we are in development, append the custom schema to the developer's custom schema prefix to isolate their work #}
+    {%- if custom_schema_name is none -%}
+        {{ target.schema }}
     {%- else -%}
-        {%- if custom_schema_name is none -%}
-            {{ target.schema }}
-        {%- else -%}
-            {{ target.schema }}_{{ custom_schema_name | trim }}
-        {%- endif -%}
+        {{ target.schema }}_{{ custom_schema_name | trim }}
     {%- endif -%}
 {%- endmacro %}
