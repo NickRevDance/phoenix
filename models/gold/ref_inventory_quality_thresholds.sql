848b9e09a5d7f53c5575864e260dd19c1a285e4a
Nick Hudson <nhudson.c@revolutiondance.com>
2026-09-17 18:15:08 +0000

    EDW-52

--- models/gold/ref_inventory_quality_thresholds.sql
+++ models/gold/ref_inventory_quality_thresholds.sql
@@ -21,7 +21,7 @@ final as (
 
     select
 
-          {{ generate_surrogate_key(['b.setting_name', 'b.scope_product_subgroup']) }} as threshold_key  -- stable per setting+scope, not per version
+          {{ generate_surrogate_key(['b.setting_name', 'b.scope_product_subgroup', 'b.effective_start_date']) }} as threshold_key  -- version-distinct key, matching ref_sla_target's pattern (natural key + effective_start_date)
 
         , b.setting_name
         , b.scope_product_subgroup
@@ -31,19 +31,10 @@ final as (
         , b.is_active
         , b.effective_start_date
         , b.effective_end_date
-        , {{ scd2_version_number('b.setting_name, b.scope_product_subgroup', order_by='b.effective_start_date') }} as version_number
-
         , b.notes
         , 'inventory_quality_thresholds_seed' as record_source
         , current_timestamp() as etl_insert_datetime
         , current_timestamp() as etl_update_datetime
-
-        , {{ generate_row_hash([
-              "coalesce(cast(b.setting_value as string), '')",
-              "coalesce(b.setting_type, '')",
-              "coalesce(cast(b.is_active as string), '')",
-              "coalesce(cast(b.effective_end_date as string), '')"
-          ]) }} as row_hash
 
     from quality_thresholds_base b
 
